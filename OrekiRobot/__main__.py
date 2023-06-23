3c02e51cce00715f1d2593261dd260adb0027e0f
LeaveEx <126212388+LeaveEx@users.noreply.github.com>
2023-06-24 06:43:43 +0700

    Update __main__.py

--- OrekiRobot/__main__.py
+++ OrekiRobot/__main__.py
@@ -290,7 +290,7 @@ def start(update: Update, context: CallbackContext):
     else:
         update.effective_message.reply_animation(
             random.choice(GROUP_START_IMG),
-            caption=f"<b>Hola, I'm awake already!\nOreki Is Here since</b>: <code>{}</code>",
+            caption=f"<b>Hola, I'm awake already!\nOreki Is Here since</b>: <code>{}</code>"
             parse_mode=ParseMode.HTML,
             reply_markup=InlineKeyboardMarkup(
                 [
